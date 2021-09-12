3e69ca37316c1df047263b4512a431b76c417646
Dennis Hernandez <dennis264@mac.com>
2021-09-11 20:18:12 -0700

    SwiftyPi update

--- Package.swift
+++ Package.swift
@@ -16,11 +16,10 @@ let package = Package(
             targets: ["ApusRubus"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-        .package(url: "https://github.com/doHernandezM/SwiftyPi.git", ._exactItem("0.2.1")),
+        .package(url: "https://github.com/doHernandezM/SwiftyPi.git", ._exactItem("0.2.2")),
 ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
