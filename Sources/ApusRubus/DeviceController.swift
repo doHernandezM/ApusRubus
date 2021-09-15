eaf62e301b720617dbcd54f2b366e028e9551174
Dennis Hernandez <dennis264@mac.com>
2021-09-15 14:07:55 -0700

    Refactor for SwiftyPi 0.4

---
+++ Sources/ApusRubus/DeviceController.swift
@@ -0,0 +1,60 @@
+//
+//  File.swift
+//
+//
+//  Created by Dennis Hernandez on 9/13/21.
+//
+
+import Foundation
+import SwiftyPi
+
+public let vaporController: VaporController = VaporController()
+
+///Controller for SwiftyPi devices.
+///
+///You can access your devices and their values directly or have the controller do it for you.
+open class DeviceController {
+    public typealias Devices = [String:SwiftyPiDevice]
+    private var devices:Devices = [:]
+    private var timer:SwiftyPi.Timer? = nil
+    private var timeInterval:TimeInterval = 0.1
+    private var loops:Int = 0
+    
+    ///Init the controller with the default time interval and loop
+    public init(devices: [SwiftyPiDevice]) {
+        self.add(devices: devices)
+    
+        do {
+            try vaporController.start()
+        }
+        catch {
+            print("caught")
+        }
+        
+    }
+    
+    ///Init the controller with custom time interval and loop
+    public init(timeInterval: TimeInterval, loops: Int, devices: [SwiftyPiDevice]) {
+        self.add(devices: devices)
+        
+        timer = Timer(timeInterval: self.timeInterval, loops: self.loops)
+        timer?.handler = { [self] in
+            action()
+        }
+    }
+    
+    ///Set off action for all of the devices.
+    func action() {
+        self.devices.forEach { _,device in
+            device.action()
+        }
+    }
+    
+    ///Add device to controller's device dictionary.
+    open func add(devices:[SwiftyPiDevice]) {
+        devices.forEach { device in
+            self.devices[device.state.name] = device
+        }
+    }
+    
+}
